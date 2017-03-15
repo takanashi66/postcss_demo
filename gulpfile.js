8523656ff63423f7a1c055850564746bb9ecd217
takanashi66 <inoue@codecodeweb.com>
2017-03-15 10:35:07 +0900

    ブラウザ追加

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,13 @@ gulp.task("browser-reload", function() {
   browserSync.reload();
 });
 
+var browsers = [
+    'last 2 version',
+    'ie 6',
+    'firefox 2',
+    'iOS >= 8.1',
+    'Android >= 4.4'
+];
 
 gulp.task('postcss', function(){
 	gulp.src(cmnpath + 'postcss/*.css')
@@ -27,9 +34,10 @@ gulp.task('postcss', function(){
 		require('postcss-selector-matches'),
 		require('postcss-custom-properties'),
 		require('postcss-nesting'),
-		require('autoprefixer'),
+		require('autoprefixer')({browsers: browsers}),
 		require('postcss-custom-media'),
-		require('css-mqpacker')
+		require('css-mqpacker'),
+    require('csswring')
 	]))
 	.pipe(gulp.dest(cmnpath + 'css'))
 	.pipe(browserSync.stream());
